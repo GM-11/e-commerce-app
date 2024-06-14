3ff0537c97eadb5a2620089a0f13dbcbcbf53669
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 19:10:52 +0530

    added connectMongo function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import cors from 'cors'; // Import cors
+import cors from "cors"; // Import cors
 import { authRouter } from "../routes/authRouter";
 import { productsRouter } from "../routes/productRouter";
 import { cartRouter } from "../routes/cartRouter";
+import connectMongo from "../database/connectMongo";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
-app.use('/', authRouter)
-app.use('/', productsRouter)
-app.use('/', cartRouter)
+connectMongo();
+
+app.use("/", authRouter);
+app.use("/", productsRouter);
+app.use("/", cartRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
