41aed193aa80ca1eb68f0dff684cb2cdc790d280
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 20:28:57 +0530

    adde products

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+import { ProductModel } from "../models/productModel";
+import ProductCard from "../components/ProductCard";
 
 function LandingPage() {
+  const [allProducts, setAllProducts] = useState<ProductModel[]>([]);
+
+  async function getAllProducts() {
+    const response = await fetch("/products", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    setAllProducts(data);
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div>
-      <h1>LANDING PAGE</h1>
+      <h1>Products</h1>
+
+      <section>
+        {allProducts.map((product) => {
+          const name = product.name;
+          const price = product.price;
+          const description = product.description;
+          const imageUrl = product.imageUrl;
+          const stock = product.stock;
+          const id = product.id;
+
+          return (
+            <ProductCard
+              key={id}
+              id={id}
+              name={name}
+              price={price}
+              description={description}
+              imageUrl={imageUrl}
+              stock={stock}
+            />
+          );
+        })}
+      </section>
+      {allProducts.length === 0 && <p>No products found</p>}
     </div>
   );
 }
