525f13b9ae559b8ab1572ba841be3bfa23297b0b
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 14:27:49 +0530

    defined product controller

---
+++ backend/controllers/productController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export async function getProducts(req: Request, res: Response) {}
+
+export async function getProduct(req: Request, res: Response) {}
+
+export async function addProduct(req: Request, res: Response) {} // admin Only
+
+export async function updateProduct(req: Request, res: Response) {} // admin Only
+
+export async function deleteProduct(req: Request, res: Response) {} // admin Only
