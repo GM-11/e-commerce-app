f8e2f52111feb62617bd6517a18083bab0e3b52a
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 14:28:11 +0530

    added routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from 'cors'; // Import cors
 import { authRouter } from "../routes/authRouter";
+import { productsRouter } from "../routes/productRouter";
+import { cartRouter } from "../routes/cartRouter";
 
 dotenv.config();
 
@@ -11,7 +13,9 @@ const port = process.env.PORT || 3000;
 app.use(cors()); 
 app.use(express.json());
 
-app.use('/auth/', authRouter)
+app.use('/', authRouter)
+app.use('/', productsRouter)
+app.use('/', cartRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
