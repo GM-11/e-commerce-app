7f83d2faaafa1bcce12cc84c8d861796f27f70d2
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 14:27:39 +0530

    defined cart controller

---
+++ backend/controllers/cartController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export async function getCart(req: Request, res: Response) {}
+
+export async function addItemToCart(req: Request, res: Response) {}
+
+export async function deleteItemFromCart(req: Request, res: Response) {} 
