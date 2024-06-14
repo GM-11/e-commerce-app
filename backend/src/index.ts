5c3b4f90add633198f543be0b28dbd378e65eb98
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 14:13:22 +0530

    added auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from 'cors'; // Import cors
+import { authRouter } from "../routes/authRouter";
 
 dotenv.config();
 
@@ -10,10 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors()); 
 app.use(express.json());
 
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use('/auth/', authRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
