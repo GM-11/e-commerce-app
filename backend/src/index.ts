32bbc6520d2be78ad6bd440be9e6a67152869a02
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 13:54:36 +0530

    added cors and express.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors'; // Import cors
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors()); 
+app.use(express.json());
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
