a55e04ec7cdcb2287630a17b27d338c443e21db9
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 20:27:50 +0530

    added image url and stock

--- backend/models/productModel.ts
+++ backend/models/productModel.ts
@@ -4,7 +4,9 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String, required: true }
+    category: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    stock: { type: Number, required: true }
 });
 
 export const Product = mongoose.model("Product", productSchema);
