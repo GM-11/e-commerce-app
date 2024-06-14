f926d4b1fa7250f04bc42753d91ac1b379401b9b
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 20:28:14 +0530

    added product model and user model type

---
+++ frontend/src/models/productModel.ts
@@ -0,0 +1,10 @@
+
+export type ProductModel = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  imageUrl: string;
+  stock: number;
+};
---
+++ frontend/src/models/userModel.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  quantity: { type: Number, required: true }
+});
+
+
+export const userSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false }, 
+  cart: [cartItemSchema]
+});
