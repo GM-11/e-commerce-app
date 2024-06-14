ebb9c54c41245242192037505217b105a5356d72
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 18:07:57 +0530

    added comment

--- backend/routes/productRouter.ts
+++ backend/routes/productRouter.ts
@@ -6,11 +6,12 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/productController";
+import { isAdmin } from "../middlewares/isAdmin";
 
 export const productsRouter = express.Router();
 productsRouter.get("/products", getProducts);
 productsRouter.get("/products/:id", getProduct);
-productsRouter.post("/products", addProduct);
-productsRouter.put("/products/:id", updateProduct);
-productsRouter.delete("/products/:id", deleteProduct);
-
+// admin only
+productsRouter.post("/products", isAdmin, addProduct);
+productsRouter.put("/products/:id", isAdmin, updateProduct);
+productsRouter.delete("/products/:id", isAdmin, deleteProduct);
