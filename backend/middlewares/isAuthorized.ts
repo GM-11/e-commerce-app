bf2d49707d97196679238aaca8c4f1061512d547
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 18:07:46 +0530

    created isAuthorized middleware

---
+++ backend/middlewares/isAuthorized.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    res.status(401).send("Access Denied");
+    return;
+  }
+  try {
+    const verified = jwt.verify(token, "secretkey");
+    req.body.userId = verified;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid Token");
+  }
+}
