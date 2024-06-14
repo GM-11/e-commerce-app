4f4e561fbc4b3ad2745a88c4dbcb4f63d225daf7
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 21:18:13 +0530

    styled product card compnent

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import "./styles/productCard.css";
+
 type props = {
   id: string;
   name: string;
@@ -8,13 +11,29 @@ type props = {
 };
 
 function ProductCard(props: props) {
-  return <div>
-    <h2>{props.name}</h2>
-    <p>{props.price}</p>
-    <p>{props.description}</p>
-    <img src={props.imageUrl} alt={props.name} />
-    <p>{props.stock}</p>
-  </div>;
+  return (
+    <Link to={`/product/${props.id}`}>
+      <div className="productCard">
+        <img src={props.imageUrl} alt={props.name} />
+        <br />
+        <p>{props.id}</p>
+        <div className="content">
+          <section className="info">
+            <h2>{props.name}</h2>
+            <p className="price">
+              Price: <strong>{props.price}</strong>/INR
+            </p>
+          </section>
+          <section className="info">
+            <p>Stock Left</p>
+            <p>{props.stock}</p>
+          </section>
+          <br />
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default ProductCard;
