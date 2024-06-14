da13205a5fe17a0ff8f1ee5d81a1c0a008bdb391
GM-11 <mathuyrgopal@gmail.com>
2024-06-14 13:54:07 +0530

    added mongo DB connection

---
+++ backend/database/connectMongo.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+
+export default async function connectMongo() {
+  try {
+    const result = await mongoose.connect(`${process.env.MONGO_URI}`, {});
+
+    if (result) {
+      console.log(result);
+      console.log("Connected with MongoDB");
+    }
+  } catch (error) {
+    console.log("Some error occured");
+    console.log(error);
+  }
+}
